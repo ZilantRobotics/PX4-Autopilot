fa2ea6831c2991ba437d2586142fce76bdcc4b5e
Dmitry Ponomarev <ponomarevda96@gmail.com>
2022-12-12 23:43:09 +0300

    Cyphal: add gyroscope

--- src/drivers/cyphal/ParamManager.hpp
+++ src/drivers/cyphal/ParamManager.hpp
@@ -120,7 +120,7 @@ public:
 private:
 
 
-	const UavcanParamBinder _uavcan_params[30] {
+	const UavcanParamBinder _uavcan_params[31] {
 		{"uavcan.pub.udral.esc.0.id",                	"UCAN1_ESC_PUB",		px4_param_to_uavcan_port_id, uavcan_port_id_to_px4_param},
 		{"uavcan.pub.udral.readiness.0.id",            	"UCAN1_READ_PUB",		px4_param_to_uavcan_port_id, uavcan_port_id_to_px4_param},
 		{"uavcan.pub.udral.servo.0.id",              	"UCAN1_SERVO_PUB",		px4_param_to_uavcan_port_id, uavcan_port_id_to_px4_param},
@@ -135,6 +135,7 @@ private:
 		{"uavcan.sub.udral.gps.status.0.id",		"UCAN1_GPSST0_SUB",		px4_param_to_uavcan_port_id, uavcan_port_id_to_px4_param},
 		{"uavcan.sub.udral.gps.pdop.0.id",		"UCAN1_GPSPD0_SUB",		px4_param_to_uavcan_port_id, uavcan_port_id_to_px4_param},
 		{"uavcan.sub.udral.gps.1.id",                	"UCAN1_GPS1_SUB",		px4_param_to_uavcan_port_id, uavcan_port_id_to_px4_param},
+		{"uavcan.sub.udral.gyro.0.id",                	"UCAN1_GYRO_SUB",		px4_param_to_uavcan_port_id, uavcan_port_id_to_px4_param},
 		{"uavcan.sub.udral.mag.0.id",               	"UCAN1_MAG0_SUB",		px4_param_to_uavcan_port_id, uavcan_port_id_to_px4_param},
 		{"uavcan.sub.udral.range.0.id",               	"UCAN1_RANGE0_SUB",		px4_param_to_uavcan_port_id, uavcan_port_id_to_px4_param},
 		{"uavcan.sub.udral.energy_source.0.id",      	"UCAN1_BMS_ES_SUB",		px4_param_to_uavcan_port_id, uavcan_port_id_to_px4_param},
@@ -155,7 +156,7 @@ private:
 		//{"uavcan.sub.bms.1.id",   "UCAN1_BMS1_SUB"},
 	};
 
-	CyphalTypeRegister _type_registers[19] {
+	CyphalTypeRegister _type_registers[20] {
 		{"uavcan.pub.udral.esc.0.type", 		"reg.udral.service.actuator.common.sp.Vector31"},
 		{"uavcan.pub.udral.readiness.0.type", 		"reg.udral.service.common.Readiness.0.1"},
 		{"uavcan.sub.zubax.feedback.0.type", 		"zubax.telega.CompactFeedback.0.1"},
@@ -179,5 +180,6 @@ private:
 
 		{"uavcan.sub.udral.range.0.type", 		"uavcan.si.sample.length.Scalar.1.0"},
 		{"uavcan.sub.udral.accel.0.type", 		"uavcan.si.sample.acceleration.Vector3.1.0"},
+		{"uavcan.sub.udral.gyro.0.type", 		"uavcan.si.sample.angular_velocity.Vector3.1.0"},
 	};
 };
